71af13930753871225287e9b3e347aeffb759b65
voidermalie <lestarfanny@gmail.com>
2023-03-31 19:45:20 +0200

    dropdown works but not individually

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import './DropDown.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const DropDown = () => {
-  const [dropDown, DropDown] = useState(true);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const aboutTitles = ['Fiabilité', 'Respect', 'Service', 'Sécurité'];
@@ -15,22 +16,26 @@ const DropDown = () => {
     "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
   ];
 
-  const dropDownButton = <FontAwesomeIcon icon={faChevronUp} />;
+  const closeButton = <FontAwesomeIcon icon={faChevronUp} />;
+  const openButton = <FontAwesomeIcon icon={faChevronDown} />;
 
   return (
-    <div className="dropdown-container">
-        {aboutTitles.map((aboutTitle, index) => (
-          <div key={index} className="dropdown-wrapper">
-          <h1>{aboutTitle}</h1>
-          <button>{dropDownButton}</button>
+    <div className="dropdown-wrapper">
+      {aboutTitles.map((aboutTitle, index) => (
+        <div key={index} className="dropdown-item">
+          <div className={`dropdown-title ${isOpen ? 'closed' : ''}`}>
+            <h1>{aboutTitle}</h1>
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? closeButton : openButton}
+            </button>
           </div>
-        ))}
-        
-          <div className='dropdown-description'>
-            {aboutDescriptions.map((aboutDescription, index) => (
-            <p key={index}>{aboutDescription}</p>
-            ))}
-          </div>
+          {isOpen && (
+            <div className="dropdown-description">
+              <p>{aboutDescriptions[index]}</p>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
