eec8073c427c683055d1252c98b005230b5fff36
voidermalie <lestarfanny@gmail.com>
2023-04-05 09:59:30 +0200

    rating simplified

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,6 +2,24 @@ import './Rating.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
+
+const Rating = ({ rating }) => {
+
+  const range = [1, 2, 3, 4, 5];
+  const stars = range.map((num) => (
+    <FontAwesomeIcon
+      icon={faStar}
+      key={num}
+      color={num <= rating ? '#FF6060' : '#E3E3E3'}
+    />
+  ));
+
+  return <div>{stars}</div>;
+};
+
+export default Rating;
+
+/*
 const Rating = ({ rating }) => {
   const stars = Array.from({ length: 5 }, (_, index) => ( //déterminer nombre d'éléments puis mapping
     <FontAwesomeIcon
@@ -14,4 +32,4 @@ const Rating = ({ rating }) => {
   return <div>{stars}</div>;
 };
 
-export default Rating;
+export default Rating;*/
