c77465a428b035e657587c86a92333dfc167602f
voidermalie <lestarfanny@gmail.com>
2023-04-11 12:49:32 +0200

    tag style + dropdown style

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,8 +1,10 @@
 import './Tag.css';
 
-const Tag = ({tags}) => {
-    const tag = tags.map(tag => {return <h3>{tag}</h3>})
-    return <>{tag}</>
+const Tag = ({ tags }) => {
+  const tag = tags.map((tag) => {
+    return <h3 className='tag-item'>{tag}</h3>;
+  });
+  return <div className='tag'>{tag}</div>;
 };
 
 export default Tag;
